c8146a003e48cd8994457e356aa5c8a971b14867
posse <posse666666@gmail.com>
2022-02-03 22:49:57 +0300

    hw2 1-4

--- src/les1/Main2.java
+++ src/les1/Main2.java
@@ -25,8 +25,6 @@ public class Main2 {
     public static void ex2() {
         int[] array = new int[8];
 
-        for (int i = 0; i < array.length; i++) { // заполнил цикл нулями
-        }
         System.out.println();
         System.out.println("ex2: ");
 
@@ -44,42 +42,60 @@ public class Main2 {
             System.out.print(mas3[i] + " ");
         }
         System.out.println();
+        doSomething(mas3);
+    }
 
+    static void doSomething(int[] mas3){
         for (int i = 0; i < mas3.length; i++) {
-            mas3[i] = (mas3[i] < 6) ? mas3[i] * 2 : mas3[i]; // если элемент =0, то 0 меняет на 1, в обратном случае - 1 меняет на 0, круто, да?))
-
+            mas3[i] = (mas3[i] < 6) ? mas3[i] * 2 : mas3[i];
             System.out.print(mas3[i] + " ");
         }
     }
 
     public static void ex4() {
-        int[] mas4 = new int[10];
+        int[] mas4 = generateArray();
+        int minNum4 = searchMin(mas4);
+        int maxNum4 = searchMax(mas4);
+        printResult(minNum4, maxNum4);
+    }
 
+    private static void printResult(int minNum4, int maxNum4) {
+        System.out.println();
+        System.out.println("минимальное значение = " + (minNum4 + 1) + " ");
+        System.out.println("максимальное значение = " + (maxNum4 + 1) + " ");
+    }
+
+    static int[] generateArray() {
+        int[] mas4 = new int[10];
         System.out.println();
         System.out.println("ex4:");
-
         for (int i = 0; i < mas4.length; i++) {
-            mas4[i] = (int) Math.round(Math.random() * 10); // как сгенерировать от -100 до +100? напиши прям здесь строчку
+            mas4[i] = (int) (Math.round(Math.random() * 200) - 100); // как сгенерировать от -100 до +100? напиши прям здесь строчку
             System.out.print(mas4[i] + " ");
         }
+        return mas4;
+    }
 
-        int min4 = mas4[0], max4 = mas4[0], minNum4 = 0, maxNum4 = 0;
-
+    static int searchMin(int[] mas4) {
+        int min4 = mas4[0], minNum4 = 0;
         for (int i = 0; i < mas4.length; i++) {
             if (min4 > mas4[i]) {
                 min4 = mas4[i];
                 minNum4 = i;
             }
+        }
+        return minNum4;
+    }
+
+    static int searchMax(int[] mas4) {
+        int max4 = mas4[0], maxNum4 = 0;
+        for (int i = 0; i < mas4.length; i++) {
             if (max4 < mas4[i]) {
                 max4 = mas4[i];
                 maxNum4 = i;
             }
-
         }
-
-        System.out.println();
-        System.out.println("максимальное значение = " + (maxNum4 + 1) + " ");
-        System.out.println("минимальное значение = " + (minNum4 + 1) + " ");
+        return maxNum4;
     }
 }
 
